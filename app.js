1d4106e1f8714381d9e3b8ae256e1b5a69865236
Frederick <fred@192-168-1-108.tpgi.com.au>
2020-03-08 23:14:14 +1000

    basic html/css/js

---
+++ app.js
@@ -0,0 +1,54 @@
+// display 'are you over 18' modal on load
+$(window).on('load',function(){
+  $('#staticBackdrop').modal('show');
+});
+
+// off-canvas menu
+
+$(document).ready(function() {
+ // executes when HTML-Document is loaded and DOM is ready
+  console.log("document is ready");
+  $('[data-toggle="offcanvas"], #navToggle').on('click', function () {
+  $('.offcanvas-collapse').toggleClass('open');
+  })
+});
+
+$('.spirit').on('click', function(){
+  $('.offcanvas-collapse').toggleClass('open');
+  $('.content').addClass('hidden');
+  $('.searchResult').removeClass('hidden');
+
+  var spiritName = this.innerText;
+  console.log(spiritName);
+
+  $.getJSON('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + spiritName, function(data) {
+    for(i=0; i<data.drinks.length; i++){
+
+    
+    var text = `<div class="row" style="padding-top: 5%;">
+                <div class="col-5">
+                  <img src="${data.drinks[i].strDrinkThumb}" class="rounded img-thumbnail p-0" style="height: 100px;" alt="...">
+                </div>
+                <div class="col-7">
+                  <p>${data.drinks[i].strDrink}</p>
+                  <a class="btn btn-primary btn-sm" href="#" role="button">more</a>
+                </div>
+                </div>`
+                
+              $("#searchResult"+i).html(text);
+    }
+    console.log(data)
+});
+})
+
+//end of off-canvas menu
+
+$('.navbar-brand').on('click', function(){
+  $('.content').removeClass('hidden');
+  $('.searchResult').addClass('hidden');
+})
+
+
+
+//dark theme
+!function(){var t,e=document.getElementById("darkSwitch");if(e){t=null!==localStorage.getItem("darkSwitch")&&"dark"===localStorage.getItem("darkSwitch"),(e.checked=t)?document.body.setAttribute("data-theme","dark"):document.body.removeAttribute("data-theme"),e.addEventListener("change",function(t){e.checked?(document.body.setAttribute("data-theme","dark"),localStorage.setItem("darkSwitch","dark")):(document.body.removeAttribute("data-theme"),localStorage.removeItem("darkSwitch"))})}}();
